88852ad88c890c1c2cb8857202190fadfb912ca7
Nasuta <Anna.nasuta@gmail.com>
2023-06-28 15:27:25 +0200

    user-list

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import UsersList from './UsersList';
+
+
+function App() {
+  return (
+    <div className="App">
+  <UsersList/>
+
+    </div>
+  );
+}
+
+export default App;
